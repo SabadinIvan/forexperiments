c5956139fb8d16e1554d3b4b000abd8007ff1e2a
IvanSabadin <sabadin.ivan.84@gmail.com>
2026-04-27 10:46:10 +0300

    test prototype

---
+++ patterns/src/prototype/Rectangle.java
@@ -0,0 +1,27 @@
+package prototype;
+
+public class Rectangle extends Shape {
+    public int width;
+    public int height;
+
+    public Rectangle() {}
+
+    public Rectangle(Rectangle rectangle) {
+        super(rectangle);
+        if (rectangle != null) {
+            this.width = rectangle.width;
+            this.height = rectangle.height;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Rectangle rectangle) || !super.equals(obj)) return false;
+        return rectangle.width == width && rectangle.height == height;
+    }
+}
